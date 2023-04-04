66778570991d7b8d3bc8d29b6a511b58990157b6
robin <robin.cobanov@hotmail.com>
2023-04-04 17:32:49 +0200

    All functionality is working.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const apiRouter = require('./routes/apiRouter')
+const cors = require('cors');
 require('dotenv').config();
+
 
 
 const app = express();
 
-
+app.use(cors()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
